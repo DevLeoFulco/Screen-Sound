8cb87b893443db80cdc92b22b8e52b5af901e5dc
DevLeoFulco <devleofulco@gmail.com>
2023-12-30 23:55:33 -0300

    Adicionado a função de avaliação

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 ﻿using System.Globalization;
+using Microsoft.Win32;
 
 String mensagemDeBoasVindas = "SEJA BEM VINDO AO SCREEN SOUND!";
 
-List<String> listaDeBandas = new List<string>{"Weslley Safadão", "Mari Fernandes", "Bon Jovi"};
+//List<String> listaDeBandas = new List<string>{"Weslley Safadão", "Mari Fernandes", "Bon Jovi"};
 
-
+Dictionary<string,List<int>> registroENotaBandas = new Dictionary<string, List<int>>();
+registroENotaBandas.Add("AC/DC",new List<int>{8,9,5});
+registroENotaBandas.Add("Iron Maden",new List<int>{10,9,7});
+registroENotaBandas.Add("The Glorian",new List<int>());
 
 
 void ExibirLogo()
@@ -39,10 +43,9 @@ void ExibirOpcoesDoMenu()
         
         break;
         case 2: mostrarBandasRegistrada();
-        
         break;
-        case 3:
-        Console.WriteLine("");
+        case 3: AvaliarBandaRegistrada();
+        
         break;
         case 4:
         Console.WriteLine("");
@@ -55,12 +58,10 @@ void ExibirOpcoesDoMenu()
 void RegistrarBandas ()
 {
     Console.Clear();
-    Console.WriteLine("**************************");
-    Console.WriteLine("REGISTRO DE BANDAS");
-    Console.WriteLine("**************************\n");
+    ExibirTituloEmOpcao("REGISTRO DE BANDAS");
     Console.Write("Digite o nome da banda que deseja registrar: ");
     String nomeDaBanda = Console.ReadLine()!;
-    listaDeBandas.Add(nomeDaBanda);
+    registroENotaBandas.Add(nomeDaBanda,new List<int>());
     Console.WriteLine($" {nomeDaBanda} foi registrada com SUCESSO!");
     Thread.Sleep(2000);
     Console.Clear();
@@ -71,13 +72,16 @@ void RegistrarBandas ()
 void mostrarBandasRegistrada()
 {
     Console.Clear();
-    Console.WriteLine("***********************************");
-    Console.WriteLine("LISTA DE BANDAS REGISTRADAS");
-    Console.WriteLine("***********************************\n");
+    ExibirTituloEmOpcao("TODAS AS BANDAS REGISTRADAS");
 
-    for (int i= 0; i<listaDeBandas.Count; i++)
+    // for (int i= 0; i<listaDeBandas.Count; i++)
+    // {
+    //     Console.WriteLine($"BANDA - {listaDeBandas[i]}");
+    // }
+
+    foreach(string banda in registroENotaBandas.Keys)
     {
-        Console.WriteLine($"BANDA - {listaDeBandas[i]}");
+        Console.WriteLine($"BANDA - {banda}");
     }
     Console.WriteLine("\nDeseja REGISTRAR uma nova banda ? digite S para Registrar ou N para voltar ao MENU PRINCIPAL");
     string digitouSim = Console.ReadLine();
@@ -88,12 +92,57 @@ void mostrarBandasRegistrada()
     {
         ExibirOpcoesDoMenu();
     }
+
+     
+    
     
     
     
     ExibirOpcoesDoMenu();
 
 }
+
+void AvaliarBandaRegistrada()
+    {
+        //Preciso que o usuario informe qual banda quer avaliar
+        //Se ela existir -> atribuir nota
+        //Se não existir perguntar se quer registrar ela como nova
+
+        Console.Clear();
+        ExibirTituloEmOpcao("Avaliação de Bandas");
+        Console.Write("Digite o nome da Banda que deseja avaliar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+
+        if (registroENotaBandas.ContainsKey(nomeDaBanda))
+        {
+                Console.Write($"Qual avaliação a banda {nomeDaBanda} merece?");
+                int nota = int.Parse(Console.ReadLine());
+                registroENotaBandas [nomeDaBanda].Add(nota);
+                Console.WriteLine($"Você registrou a nota {nota} com SUCESSO para a banda {nomeDaBanda}");
+        }else 
+        {
+            Console.WriteLine($"{nomeDaBanda} não existe na lista atual de bandas! Deseja Registrar? (S) ou (N) ");
+            string digitouSim = Console.ReadLine();
+
+            if (digitouSim == "S")
+             {
+                RegistrarBandas();
+            }else if (digitouSim == "N") 
+            {
+                ExibirOpcoesDoMenu();
+            }
+
+        }
+
+    }
+void ExibirTituloEmOpcao(String titulo)
+    {
+        int quantidadeDeLetras = titulo.Length;
+        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras,'*');
+        Console.WriteLine(asteriscos);
+        Console.WriteLine(titulo);
+        Console.WriteLine(asteriscos + "\n");
+    }
 
 ExibirLogo();
 ExibirOpcoesDoMenu();
